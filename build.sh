f8d1930a39d409a59cbd116d928b19a53d06fe9e
Haitao Li <lihaitao@gmail.com>
2017-03-18 18:40:47 +1100

    Push images in script step

--- build.sh
+++ build.sh
@@ -6,15 +6,21 @@ if [ -z "$DATE" ]; then
   DATE=`date +%Y-%m-%d`
 fi
 
-TAG="${DATE}"
-
 LINK="${SITE}/${DATE}/rust-nightly-x86_64-unknown-linux-gnu.tar.gz"
 echo $LINK
 
 # test if there is a nightly build
 if `curl --fail -I ${LINK} >/dev/null 2>&1`; then
-    docker build --build-arg BUILD_DATE=${DATE} -t htli/rust-nightly:${TAG} .
+    docker build --build-arg BUILD_DATE=${DATE} -t htli/rust-nightly:${DATE} .
     docker images | grep rust-nightly
+
+    if [ "$TRAVIS_BRANCH" == "master" ]; then
+        docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+        docker push "htli/rust-nightly:${DATE}";
+        docker tag "htli/rust-nightly:${DATE}" "htli/rust-nightly:latest";
+        docker push "htli/rust-nightly:latest";
+    fi
+
 else
     echo "No nightly build on ${DATE}"
 fi
--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-  docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-  docker push "htli/rust-nightly:${DATE}";
-  docker tag "htli/rust-nightly:${DATE}" "htli/rust-nightly:latest";
-  docker push "htli/rust-nightly:latest";
-fi
