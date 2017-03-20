e4a89250c5a23165d0724923322af8103be778db
Haitao Li <lihaitao@gmail.com>
2017-03-20 16:42:13 +1100

    Revert "Build a nodocs variant"
    This reverts commit 6696816fba8999bc4b78cec1eb01c7155f08790f.

--- build.sh
+++ build.sh
@@ -1,37 +1,24 @@
 #!/bin/bash
 
-set -e
-
 SITE="https://static.rust-lang.org/dist"
 
 if [ -z "$DATE" ]; then
   DATE=`date +%Y-%m-%d`
 fi
 
-IMAGE="htli/rust-nightly"
-IMAGE_DATE="${IMAGE}:${DATE}"
-IMAGE_DATE_NODOCS="${IMAGE}:${DATE}-nodocs"
-
 LINK="${SITE}/${DATE}/rust-nightly-x86_64-unknown-linux-gnu.tar.gz"
-DOC_LINK="${SITE}/${DATE}/rust-docs-nightly-x86_64-unknown-linux-gnu.tar.gz"
+echo $LINK
 
 # test if there is a nightly build
 if `curl --fail -I ${LINK} >/dev/null 2>&1`; then
-    docker build --build-arg BUILD_DATE="${DATE}" -t "${IMAGE_DATE_NODOCS}" .
-    docker tag "${IMAGE_DATE_NODOCS}" "${IMAGE}:latest-nodocs";
-
+    docker build --build-arg BUILD_DATE=${DATE} -t htli/rust-nightly:${DATE} .
     docker images | grep rust-nightly
-
-    sed s/%DATE%/${DATE}/g Dockerfile.docs.in > Dockerfile.docs
-    docker build -t ${IMAGE}:${DATE} -f Dockerfile.docs .
-    docker tag ${IMAGE}:${DATE} ${IMAGE}:latest
 
     if [ "$TRAVIS_BRANCH" == "master" ]; then
         docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-        docker push "${IMAGE}:${DATE}";
-        docker push "${IMAGE}:${DATE}-nodocs";
-        docker push "${IMAGE}:latest";
-        docker push "${IMAGE}:latest-nodocs";
+        docker push "htli/rust-nightly:${DATE}";
+        docker tag "htli/rust-nightly:${DATE}" "htli/rust-nightly:latest";
+        docker push "htli/rust-nightly:latest";
     fi
 
 else
